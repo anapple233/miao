6644f9db29eb0ef7f6f05bca7072c7f2f4809754
qiqi <2232399616@qq.com>
2025-06-20 21:55:47 +0800

    ladash - 2

--- lodash/1anapple-lodash.js
+++ lodash/1anapple-lodash.js
@@ -1,5 +1,25 @@
 var foobar = {
-  chunk: function(){
+  compact : function (array) {
+    return array.filter((item) => {
+      return typeof(item) == 'number'
 
+    })
+  },
+  chunk: function(array, size = 1){
+    let result = []
+    let flag = 0
+    let arr = []
+    let len = array.length
+    array.forEach((item, idx) => {
+      if (flag == size) {
+        result.push(arr)
+        flag = 0
+        arr = []
+      }
+      arr.push(item)
+      flag++
+    })
+    result.push(arr)
+    return result
   }
 }
