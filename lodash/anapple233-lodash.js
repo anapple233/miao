f889d223c32ce717771cf13d2f569f68cc270f89
qiqi <2232399616@qq.com>
2025-06-27 11:35:49 +0800

    ladash-4

--- lodash/anapple233-lodash.js
+++ lodash/anapple233-lodash.js
@@ -1,7 +1,17 @@
+/*
+ compact,chunk,fill,drop,findIndex,findLastIndex,flatten,flattenDeep,flattenDepth
+        fromPairs,toPairs,head,indexOf,lastIndexOf,initial,join,last,pull,reverse,every,some
+        countBy,groupBy,keyBy,forEach,map,filter,reduce,reduceRight,size,sortBy,sample,
+        isUndefined,isNull,isNil,max,min,maxBy,minBy,round,sumBy
+        flagMap,flatMapDepth,get,has,mapKeys,mapValues
+        range,stringifyJSON,concat,isEqual,repeat,padStart,padEnd,pad,keys,values,random,
+        round,ceil,floor,cloneDeep
+        trim,trimStart,trimEnd,assign,merge,
+        */
 var anapple233 = {
   compact : function (array) {
     return array.filter((item) => {
-      return typeof(item) == 'number'
+      return typeof(item) == 'number' && !Number.isNaN(item)  && item != 0
     })
   },
   chunk: function(array, size = 1){
@@ -20,5 +30,30 @@ var anapple233 = {
     })
     result.push(arr)
     return result
+  },
+  fill: function(array, value) {
+    let len = array.length
+    return new Array(len).fill(value)
+  },
+  drop: function(array, n = 1) {
+    let i = n
+    let len = array.length
+    let result = []
+    while (i < len) {
+      result.push(array[i])
+      i++
+    }
+    return result
+  },
+  findIndex: function(users, callback, fromIndex = 0) {  //这里又接函数又接参数是怎么写的
+    for (let user in users) {
+      return function (o) {
+        if(callback(o)) {
+
+        }
+
+      }
+
+    }
   }
 }
